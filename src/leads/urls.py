2bf96aaed3eeafa66308070d9969ce1872e0c6da
jatin77 <clockstopfountain@gmail.com>
2019-12-15 17:51:13 +0530

    temp routes solution

--- src/leads/urls.py
+++ src/leads/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.conf.urls import url
-
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
     path('', include('posts.urls')),
     path('', include('accounts.urls')),
-    url(r'^.*', TemplateView.as_view(template_name="index.html"), name="index"),
-
+    # url(r'^.*', TemplateView.as_view(template_name="index.html"), name="index"),
 ]
--- src/leads/views.py
+++ src/leads/views.py
@@ -1,14 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.http import HttpResponse, JsonResponse
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.utils import json
-from rest_framework import serializers
-from rest_framework import viewsets
 
 
-class HomePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+def index(request):
+    return render(request, 'index.html')
