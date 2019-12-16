3b04236d01668a9597e349b850ad94d82a23381f
jatin77 <clockstopfountain@gmail.com>
2019-12-16 15:23:09 +0530

    asd

--- leads/leads/settings.py
+++ leads/leads/settings.py
@@ -1,6 +1,8 @@
 
 
 import os
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,3 +131,6 @@ STATICFILES_DIRS = [
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+django_heroku.settings(locals())
